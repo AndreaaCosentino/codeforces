4307b59cd42137e4a9eab31c0db4002a7887a8d1
AndreaaCosentino <110279497+AndreaaCosentino@users.noreply.github.com>
2022-08-13 21:42:38 +0200

    Create ProblemB.c
    Problem B of Contest #813 Div. 2

---
+++ 2022/#814_Div2/ProblemB.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		int n;
+		scanf("%d",&n);
+		int v[n];
+		v[0] = 1;
+		for(int i = n-1; i > 0; i -= 2)
+		{
+			v[i] = i;
+			v[i-1] = i+1;
+		}
+		for(int i = 0; i < n ; i++)
+			printf("%d ",v[i]);
+		printf("\n");
+	}
+	return 0;
+}
